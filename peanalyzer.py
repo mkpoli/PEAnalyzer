6ed1a434faad4b9ee4b683656fcd1b87811e15b8
mkpoli <snowwolf1001@gmail.com>
2016-06-30 17:55:39 +0800

    Added features
    Can tell a file's times. Can tell a file has 'MZ' or not. Can tell PE
    Pointer. Some TODO

--- peanalyzer.py
+++ peanalyzer.py
@@ -1,7 +1,9 @@
 import os.path
+import textwrap
+import time
 
 # Startup Printing
-VERSION = '0.0.0.1'
+VERSION = '0.0.0.2'
 print('PE Analyzer [Version ' + VERSION + ']')
 print('2016 by mkpoli.')
 print()
@@ -13,9 +15,41 @@ while True:
         break
     else :
         print('Error: Not a file. Please reinput.')
-        
+
+# Processing
+# TODO: wrap text
+
 # Basic File Info
+# TODO: Use os.stat
+# TODO: import python-magic
+TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+fsize = os.path.getsize(fname)
+fatime = os.path.getatime(fname)
+fmtime = os.path.getmtime(fname)
+fctime = os.path.getctime(fname)
+
 print('-- File Information -- ',
 '      File Name = ' + fname,
-'      File Size = ' + str(os.path.getsize(fname)) + ' Bytes'
-, sep='\n')
+'      File Size = ' + str(fsize) + ' Bytes',
+'      Last Access Time = ' + str(fatime) + ', in UTC = ' + time.strftime(TIME_FORMAT, time.gmtime(fatime)) + ', in Local = ' + time.strftime(TIME_FORMAT, time.localtime(fatime)),
+'      Last Modify Time = ' + str(fmtime) + ', in UTC = ' + time.strftime(TIME_FORMAT, time.gmtime(fmtime)) + ', in Local = ' + time.strftime(TIME_FORMAT, time.localtime(fmtime)),
+'      Last Create Time = '  + str(fctime) + ', in UTC = ' + time.strftime(TIME_FORMAT, time.gmtime(fctime))+ ', in Local = ' + time.strftime(TIME_FORMAT, time.localtime(fctime)),
+sep='\n')
+
+# File Stream Processing
+with open(fname, "rb") as f:
+    magicn = f.read(2)
+    hasMZ = (magicn == b'MZ')
+    if not hasMZ :
+        print('-- PE Information -- ',
+        '     Has Valid Magic Number "MZ" = ' + str(hasMZ),
+        '',
+        'This is NOT a PE File. Stopping Processing.',
+        sep='\n')
+        exit()
+    f.read(0x3A)
+    pepointer = f.read(1)
+    print('-- PE Information -- ',
+    '     Has Valid Magic Number "MZ" = ' + str(hasMZ),
+    '     PE Pointer = ' + str(pepointer),
+    sep='\n')
